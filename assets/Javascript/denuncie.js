03a9603db984df292bbf9d081082dc992820ade3
jpbrandaodev <joaopedrobrandaodev>
2026-06-26 12:17:56 -0300

    feat:sistema de denuncias PT1

--- assets/Javascript/denuncie.js
+++ assets/Javascript/denuncie.js
@@ -1,30 +1,59 @@
 import { db, getDatabase, ref, set } from './firebaseconfig'
 
-/* exemplo 
+// Quem pegou pra ver esse Js agora, provavelmente ele tá
+// incompleto pq eu não aprendi enviar pro banco de dados ainda.
 
-let teste = {
-    nome:'',
-    topico:'',
-    comentario: ''
+let resolvido = false;
+let naoResolvido = false;
+const regex = /www|http|https/i;
+
+function marcarResolvido(){
+    resolvido = true;
+    NaoResolvido = false;
 }
-*/
 
-function enviarDenuncia() {
+function marcarNaoResolvido(){
+    resolvido = false;
+    naoResolvido = true;
+}
 
-    const topico = document.getElementById("topico").value;
-    const problema = document.getElementById("problema").value;
+function enviarDenuncia(){
+    const topico = document.getElementById("topico").value.trim();
+    const problema = document.getElementById("problema").value.trim();
 
-    const regex = /www|http|https/i;
-
+    if (topico === "" || problema === ""){
+        alert("Preencha todos os campos!")
+        return;
+    }
     if (regex.test(topico) || regex.test(problema)) {
         alert("Não é permtido enviar links!")
         return;
     }
-
-    const denuncias = {
-        topico = null,
-        problema = null,
-        like = null,
-        dislike = null,
+    if (resolvido === false && NaoResolvido === false){
+        alert("Marque se o problema foi resolvido ou não!")
+        return;
     }
+    const denuncia = {
+        topico: topico,
+        problema: problema,
+        resolvido: resolvido,
+        naoResolvido: naoResolvido,
+    };
+
+    console.log(denuncia);
+
+    /*
+    Aqui vai ficar as coisas do banco de dados
+    que eu falei que não sei fazer porém irei
+    aprender
+
+    NÂO ME EXPULSEM PLISSS
+    */
+
+    // Dps de enviar ele limpa os campos
+    document.getElementById("topico").value ="";
+    document.getElementById("problema").value ="";
+    resolvido = false;
+    naoResolvido = false;
+
 }
