ec45abf612c94dcf67a65a6a4b6610a1744a9185
jpbrandaodev <joaopedrobrandaodev>
2026-06-28 22:03:51 -0300

    sistema de pesquisa pt1

---
+++ assets/Javascript/denuncias.js
@@ -0,0 +1,44 @@
+import { db, collection } from './firebaseconfig.js';
+import { query, where, getDocs } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+
+const form = document.getElementById('pesquisa');
+const pesquisa = document.getElementById('pesquisaUsuario');
+
+form.addEventListener('submit', (evento) => {
+    evento.preventDefault();
+
+    const termoPesquisa =  pesquisa.value.trim();
+    
+    if(termoPesquisa === ''){
+        // alert("Digite algo para pesquisar!")
+        return;
+    }
+
+    buscarDenunciaFeita(termoPesquisa);
+});
+
+async function buscarDenunciaFeita(termoPesquisa){
+    console.log(`Buscando por: ${termoPesquisa}`);
+    
+    try {
+        const resultado = await getDocs(collection(db, "denuncias"));
+        
+        resultado.forEach((doc) => {
+            const dados = doc.data();
+            if (dados.topico === termoPesquisa) {
+                console.log(dados);
+                // AQUI VAI ENTRAR O INNERHTML
+            }
+        });
+
+    } catch (erro) {
+        console.error(erro);
+    }
+};
+
+// Coisas a fazer aqui
+/*
+Barra de pesquisa
+innerHTML - pras denuncias
+mostrar oq o usuario pesquisou na barra de pesquisa
+*/
