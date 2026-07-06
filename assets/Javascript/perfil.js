1374ef430e8289bd69db7715f2a7cab22d85a970
olv_sophi <deoliveiras086@gmail.com>
2026-07-05 23:46:00 -0300

    fix: sincroniza renomeação da pasta javascript

--- assets/Javascript/perfil.js
+++
@@ -1,6 +0,0 @@
-import { auth, db, onAuthStateChanged, signOut } from "./firebase-config.js"; 
-import { doc, getDoc, updateDoc } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
-
-// ele vai pegar as informações do usuario do banco de dados
-// e vai colocar nas coisas aq
-
